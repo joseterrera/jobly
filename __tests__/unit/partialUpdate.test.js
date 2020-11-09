cf801851fec96352507e04f505065cdbcf0e823f
Jose Terrera <joseterrera@yahoo.com>
2020-11-08 19:47:23 -0800

    added packages, added table, about to add routes

--- __tests__/unit/partialUpdate.test.js
+++ __tests__/unit/partialUpdate.test.js
@@ -1,9 +1,19 @@
+const sqlForPartialUpdate = require("../../helpers/partialUpdate");
+
 describe("partialUpdate()", () => {
   it("should generate a proper partial update query with just 1 field",
       function () {
-
-    // FIXME: write real tests!
-    expect(false).toEqual(true);
+        const {query, values } = sqlForPartialUpdate(
+          "users",
+          { first_name: "Test" },
+          "username",
+          "testuser"
+        );
+        expect(query).toEqual(
+          "UPDATE users SET first_name=$1 WHERE username=$2 RETURNING *"
+      );
+  
+      expect(values).toEqual(["Test", "testuser"]);
 
   });
 });
---
+++ readme.md
@@ -0,0 +1,11 @@
+add gitignore
+npm init
+npm install express bcypt json schema, jsonwebtoken morgan pg
+create two databases one to test
+
+adding unit test for partial update, check this step
+
+
+what does returning do?
+The RETURNING clause allows you to retrieve values of columns (and expressions based on columns) that were modified by an insert, delete or update. Without RETURNING, you would have to run a SELECT statement after the DML statement is completed, in order to obtain the values of the changed columns. So, RETURNING helps avoid another roundtrip to the database, another context switch in a PL/SQL block.
+
