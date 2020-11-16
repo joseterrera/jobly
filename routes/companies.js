ed48ab9d4b3e1bdbdb51b7ef51813d9897333918
Jose Terrera <joseterrera@yahoo.com>
2020-11-15 21:44:38 -0800

    adding routes

--- routes/companies.js
+++ routes/companies.js
@@ -1,3 +1,5 @@
+/** Routes for companies. */
+
 const express = require('express');
 const ExpressError = require('../helpers/ExpressError');
 const { authRequired, adminRequired } = require('../middleware/auth');
@@ -7,69 +9,72 @@ const { companyNewSchema, companyUpdateSchema } = require('../schemas');
 
 const router = new express.Router();
 
+/** GET /  =>  {companies: [company, company]}  */
 
-/** GET   { companies: [company, company ] }*/
-router.get('/', authRequired, async function(req, res,next) {
+router.get('/', authRequired, async function(req, res, next) {
   try {
     const companies = await Company.findAll(req.query);
-    return res.json( {companies} )
+    return res.json({ companies });
   } catch (err) {
     return next(err);
   }
 });
 
-/** GET /[handle]  => { company: company } */
+/** GET /[handle]  =>  {company: company} */
 
 router.get('/:handle', authRequired, async function(req, res, next) {
   try {
     const company = await Company.findOne(req.params.handle);
-    return res.json( { company });
-  } catch( err ) {
+    return res.json({ company });
+  } catch (err) {
     return next(err);
   }
-
 });
 
-/** POST / {companyData} => { company: newCompany } */
+/** POST / {companyData} =>  {company: newCompany} */
 
 router.post('/', adminRequired, async function(req, res, next) {
   try {
-    const validation = validate(req.body.companyNewSchema);
+    const validation = validate(req.body, companyNewSchema);
+
     if (!validation.valid) {
       throw new ExpressError(validation.errors.map(e => e.stack), 400);
     }
 
     const company = await Company.create(req.body);
-    return res.status(201).json( {company} );
-  } catch(err) {
+    return res.status(201).json({ company }); // 201 CREATED
+  } catch (err) {
     return next(err);
   }
 });
 
-/** PATCH /[handle] {comoanyData} => { company: updateCompany } */
+/** PATCH /[handle] {companyData} => {company: updatedCompany}  */
 
 router.patch('/:handle', adminRequired, async function(req, res, next) {
   try {
-    //the handle would the alternative to an id, something added upon creation that cannot be changed.
-    if ('handle' in req.body ) {
-      throw new ExpressError('You are not allowed to change the handle', 400)
+    if ('handle' in req.body) {
+      throw new ExpressError('You are not allowed to change the handle.', 400);
     }
-    const validation = validate(req.body.companyUpdateSchema);
-    if(!validation.valid) {
-      throw new ExpressError(validation.errors.map( e => e.stack), 400);
+
+    const validation = validate(req.body, companyUpdateSchema);
+    if (!validation.valid) {
+      throw new ExpressError(validation.errors.map(e => e.stack), 400);
     }
+
     const company = await Company.update(req.params.handle, req.body);
-    return res.json( { company });
-  } catch(err) {
+    return res.json({ company });
+  } catch (err) {
     return next(err);
   }
 });
+
+/** DELETE /[handle]  =>  {message: "Company deleted"}  */
 
 router.delete('/:handle', adminRequired, async function(req, res, next) {
   try {
     await Company.remove(req.params.handle);
-    return res.json({ message: 'Company deleted'});
-  } catch(err) {
+    return res.json({ message: 'Company deleted' });
+  } catch (err) {
     return next(err);
   }
 });
